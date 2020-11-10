09d5246db9ee5500227ab51469925126b9c076ec
hexdump <hexdump0815@googlemail.com>
2020-11-10 10:07:49 +0100

    some more cleanup and tuning

--- misc.ah3/options/enable-additional-options.sh
+++ misc.ah3/options/enable-additional-options.sh
@@ -6,6 +6,14 @@ cd /compile/source/linux-orangepi
 ./scripts/config -d CONFIG_EXT3_FS
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0
 ./scripts/config -d CONFIG_SUNXI_EXT_PHY
+./scripts/config -d CONFIG_SUNXI_EPHY
+./scripts/config -d CONFIG_SPARD_WLAN_SUPPORT
+./scripts/config -d CONFIG_SC23XX
+./scripts/config -d CONFIG_AW_WIFI_DEVICE_UWE5622
+./scripts/config -d CONFIG_AW_BIND_VERIFY
+./scripts/config -d CONFIG_WLAN_UWE5622
+./scripts/config -d CONFIG_SPRDWL_NG
+./scripts/config -d CONFIG_TTY_OVERY_SDIO
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_SIZE
--- misc.ah6/options/enable-additional-options.sh
+++ misc.ah6/options/enable-additional-options.sh
@@ -5,8 +5,13 @@ cd /compile/source/linux-orangepi
 ./scripts/config -d CONFIG_EXT2_FS
 ./scripts/config -d CONFIG_EXT3_FS
 ./scripts/config --set-val CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE 0
-# this results in a kernel panic if disabled and not enabled in the u-boot dtb
-#./scripts/config -d CONFIG_SUNXI_EXT_PHY
+./scripts/config -d CONFIG_SPARD_WLAN_SUPPORT
+./scripts/config -d CONFIG_SC23XX
+./scripts/config -d CONFIG_AW_WIFI_DEVICE_UWE5622
+./scripts/config -d CONFIG_AW_BIND_VERIFY
+./scripts/config -d CONFIG_WLAN_UWE5622
+./scripts/config -d CONFIG_SPRDWL_NG
+./scripts/config -d CONFIG_TTY_OVERY_SDIO
 ./scripts/config -d CONFIG_BLK_DEV_RAM
 ./scripts/config -d CONFIG_BLK_DEV_COUNT
 ./scripts/config -d CONFIG_BLK_DEV_SIZE
